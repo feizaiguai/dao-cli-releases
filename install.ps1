038f813186342c705fdbfb7a1ee420bfa70764ad
bigbao <196408245@qq.com>
2026-05-25 00:41:00 +0800

    feat: release binaries and staging for v1.4.10

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 # Usage: irm https://raw.githubusercontent.com/feizaiguai/dao-cli-releases/main/install.ps1 | iex
 
 param(
-    [string]$Version = "1.4.9",
+    [string]$Version = "1.4.10",
     [string]$InstallDir = "$env:LOCALAPPDATA\Programs\dao-cli"
 )
 
