cdd5a2060f73e1c6e66c3b2e7e84c39381c1649a
bigbao <196408245@qq.com>
2026-05-25 06:24:21 +0800

    release: 发布 v1.6.0 终极纯净汉化版二进制资产

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 # Usage: irm https://raw.githubusercontent.com/feizaiguai/dao-cli-releases/main/install.ps1 | iex
 
 param(
-    [string]$Version = "1.5.9",
+    [string]$Version = "1.6.0",
     [string]$InstallDir = "$env:LOCALAPPDATA\Programs\dao-cli"
 )
 
