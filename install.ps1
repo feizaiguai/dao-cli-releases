3287b264908c9f83ad1036fd10be81507fa70432
feizaiguai <128154130+feizaiguai@users.noreply.github.com>
2026-06-02 17:20:50 +0800

    release: update installer to v1.8.20

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 # Install command: irm https://raw.githubusercontent.com/feizaiguai/dao-cli-releases/main/install.ps1 | iex
 
 param(
-    [string]$Version = "1.8.18",
+    [string]$Version = "1.8.20",
     [string]$InstallDir = "$env:LOCALAPPDATA\Programs\dao-cli"
 )
 
