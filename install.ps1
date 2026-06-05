0895d0263f4e31ced955784f32d642044720b70f
bigbao <196408245@qq.com>
2026-06-06 02:13:02 +0800

    release dao-cli v1.9.0

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 # Usage: irm https://raw.githubusercontent.com/feizaiguai/dao-cli-releases/main/install.ps1 | iex
 
 param(
-    [string]$Version = "1.8.37",
+    [string]$Version = "1.9.0",
     [string]$InstallDir = "$env:LOCALAPPDATA\Programs\dao-cli"
 )
 
